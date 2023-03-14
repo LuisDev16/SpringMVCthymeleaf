11e304ad5a88bdd0a2beeff02558e95f67caac42
LuisBrian <luisdcm_18@gmail.com>
2023-03-14 13:15:41 -0500

    demo 01 aplicacion web con spring boot

---
+++ src/main/java/com/examplemvc/example_16/Example16Application.java
@@ -0,0 +1,13 @@
+package com.examplemvc.example_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplemvc/example_16/Example16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplemvc.example_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
