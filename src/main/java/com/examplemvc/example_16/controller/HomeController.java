9c435c1a0492ce369965bc2c98233243080a281d
LuisBrian <luisdcm_18@gmail.com>
2023-03-15 13:16:46 -0500

    desarrollo de web scholl

--- src/main/java/com/examplemvc/example_16/controller/HomeController.java
+++ src/main/java/com/examplemvc/example_16/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.examplemvc.example_16.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value={"","/","/home"})
-    public String displayHomePage(Model model){
-        model.addAttribute("username","speedrexi");
+    public String displayHomePage(){
         return "home.html";
     }
 }
